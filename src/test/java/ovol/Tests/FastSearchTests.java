f1d4c07f4b68a51f385943f085f26fff66813081
unknown <ovo@sheridan.softservecom.com>
2015-12-29 20:20:53 +0200

    2

--- src/test/java/ovol/Tests/FastSearchTests.java
+++ src/test/java/ovol/Tests/FastSearchTests.java
@@ -1,7 +1,21 @@
 package ovol.Tests;
 
+import Nhrytsko.WebDriver.Pages.LoginPage;
+import Nhrytsko.WebDriver.Pages.MainPage;
+import Nhrytsko.WebDriver.Tests.TestBase;
+import org.testng.annotations.BeforeClass;
+
 /**
  * Created by ovo on 28.12.2015.
  */
-public class FastSearchTests {
+public class FastSearchTests extends TestBase{
+    LoginPage loginPage;
+    MainPage mainPage;
+
+    @BeforeClass(alwaysRun = true)
+    public void warmUpTest() {
+        this.loginPage = new LoginPage(super.driver);
+
+
+    }
 }
