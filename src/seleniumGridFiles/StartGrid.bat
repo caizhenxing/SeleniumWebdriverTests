d71051eaf664732a24d9c2369936f25072eca537
unknown <ylysov@TEXAS.softservecom.com>
2016-01-05 15:20:26 +0200

    updated StartGrid.bat

--- src/seleniumGridFiles/StartGrid.bat
+++ src/seleniumGridFiles/StartGrid.bat
@@ -4,7 +4,7 @@ start java -jar selenium-server-standalone-2.48.2.jar -role hub
 
 
 
-start java -jar selenium-server-standalone-2.48.2.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName="internet explorer",version=ANY,platform=WINDOWS,maxInstances=1 -Dwebdriver.ie.driver="D:\JavaProject\SeleniumWebdriverTests\src\seleniumGridFiles\IEDriverServer.exe"
+start java -jar selenium-server-standalone-2.48.2.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName="internet explorer",version=ANY,platform=WINDOWS,maxInstances=1 -Dwebdriver.ie.driver="%CD%\IEDriverServer.exe"
 
 
 
