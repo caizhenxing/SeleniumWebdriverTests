2841e36ce18948645a08dbe07a715ad3d3ec83fd
unknown <ylysov@TEXAS.softservecom.com>
2016-01-05 14:53:44 +0200

    added seleniumGridFiles

---
+++ src/seleniumGridFiles/StartGrid.bat
@@ -0,0 +1,17 @@
+
+
+start java -jar selenium-server-standalone-2.48.2.jar -role hub   
+
+
+
+start java -jar selenium-server-standalone-2.48.2.jar -role webdriver -hub http://localhost:4444/grid/register -browser browserName="internet explorer",version=ANY,platform=WINDOWS,maxInstances=1 -Dwebdriver.ie.driver="D:\JavaProject\SeleniumWebdriverTests\src\seleniumGridFiles\IEDriverServer.exe"
+
+
+
+start java -jar selenium-server-standalone-2.48.2.jar -role webdriver -hub http://localhost:4444/grid/register -port 5556 -browser browserName=firefox,maxInstances=1
+
+
+
+
+
+
