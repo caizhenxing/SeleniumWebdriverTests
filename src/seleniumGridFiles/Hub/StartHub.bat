1becaf8d49ab7396a7dcedbcdbc57cf8128f8026
Nazar <nhrytsko@PAPAYA.softservecom.com>
2016-02-05 16:38:29 +0200

    Added Configuration and APK files

---
+++ src/seleniumGridFiles/Hub/StartHub.bat
@@ -0,0 +1,1 @@
+start java -jar selenium-server-standalone-2.48.2.jar -role hub -hubConfig HubConfig.json -debug
---
+++ src/seleniumGridFiles/Hub/StartLocalhostNode.bat
@@ -0,0 +1,1 @@
+start java -jar selenium-server-standalone-2.48.2.jar -role node -hub http://localhost:4444/grid/register -nodeConfig LocalhostNodeConfiguration.json
